c14761b7d9082d31d6d1f78b0d90ce97091116d5
Katie McLaughlin <katie@glasnt.com>
2015-11-21 19:19:05 +1100

    Move glasnt/octohat to LABHR/octohatrack

--- setup.py
+++ setup.py
@@ -4,29 +4,26 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'DESCRIPTION.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='octohat',
-    version='0.1.3',
+    name='octohatrack',
+    version='0.2.0',
     description='Non-code contribution groker for GitHub',
     long_description=long_description,
-    url='https://github.com/glasnt/octohat',
+    url='https://github.com/labhr/octohatrack',
     author='Katie McLaughlin',
     author_email='katie@glasnt.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3',
     ],
-    keywords='octohat github contributions non-code',
+    keywords='octohatrack github contributions non-code',
     install_requires=['requests', 'simplejson'],
     entry_points={
-      'console_scripts': [ "octohat = octohat:main" ]
+      'console_scripts': [ "octohatrack = octohatrack:main" ]
     },
     packages=find_packages()
 )
