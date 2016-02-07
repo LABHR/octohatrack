a0abf726e024342e85f87d35bea9de3fbcd8d1c4
Thomas A Caswell <tcaswell@gmail.com>
2016-02-07 15:28:25 -0500

    STY: unify indentations
    Previously had a mix of 2, 3, and 4 space indentation.
    
    Also includes line wraps + other minor style changes

--- octohatrack/__init__.py
+++ octohatrack/__init__.py
@@ -2,18 +2,26 @@
 
 import argparse
 import sys
-from .helpers import * 
+from .helpers import (generate_html, repo_exists, get_code_commentors,
+                      get_code_contributors, display_user_name)
 
-def main(): 
+
+def main():
 
   parser = argparse.ArgumentParser()
   parser.add_argument("repo_name", help="githubuser/repo")
-  parser.add_argument("-g", "--generate-html", action='store_true', help="Generate output as HTML")
-  parser.add_argument("-l", "--limit", help="Limit to the last x Issues/Pull Requests", type=int, default=0)
-  parser.add_argument("-c", "--show-contributors", action='store_true', help="Output the code contributors")
-  parser.add_argument("-n", "--show-names", action='store_true', help="Show the user's display name")
-  parser.add_argument("--no-cache", action='store_false', help='Disable local caching of API results')
-  args = parser.parse_args()  
+  parser.add_argument("-g", "--generate-html", action='store_true',
+                      help="Generate output as HTML")
+  parser.add_argument("-l", "--limit",
+                      help="Limit to the last x Issues/Pull Requests",
+                      type=int, default=0)
+  parser.add_argument("-c", "--show-contributors", action='store_true',
+                      help="Output the code contributors")
+  parser.add_argument("-n", "--show-names", action='store_true',
+                      help="Show the user's display name")
+  parser.add_argument("--no-cache", action='store_false',
+                      help='Disable local caching of API results')
+  args = parser.parse_args()
 
   repo_name = args.repo_name
 
@@ -30,22 +38,24 @@ def main():
 
   non_code_contributors = []
   for user in code_commentors:
-    user_name, avatar, name = user 
+    user_name, avatar, name = user
     if user not in code_contributors:
       non_code_contributors.append(user)
 
-  code_contributors = sorted(code_contributors, key=lambda k: k['user_name'].lower()) 
-  non_code_contributors = sorted(non_code_contributors, key=lambda k: k['user_name'].lower()) 
+  code_contributors = sorted(code_contributors,
+                             key=lambda k: k['user_name'].lower())
+  non_code_contributors = sorted(non_code_contributors,
+                                 key=lambda k: k['user_name'].lower())
 
   print("\nCode contributions: %d" % len(code_contributors))
 
   if args.show_contributors:
-    for user in code_contributors: 
+    for user in code_contributors:
       display_user_name(user, args)
-  
+
   print("\nNon-code contributions: %d" % len(non_code_contributors))
 
-  for user in non_code_contributors: 
+  for user in non_code_contributors:
     display_user_name(user, args)
 
   if args.generate_html:
@@ -53,4 +63,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+  main()
