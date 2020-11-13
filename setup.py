987c03daf65c9653a2afc12defe844fbea1a0780
Katie McLaughlin <katie@glasnt.com>
2020-11-14 10:23:00 +1100

    versioneer, black

--- setup.py
+++ setup.py
@@ -2,39 +2,35 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 import sys
+import versioneer
 
 # Exit unless we're in pip3/Python 3
 if not sys.version_info[0] == 3:
-    print("\noctohatrack requires a Python 3 environment.\n\nTry `pip3 install` instead")
+    print(
+        "\noctohatrack requires a Python 3 environment.\n\nTry `pip3 install` instead"
+    )
     sys.exit(1)
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+with open(path.join(here, "DESCRIPTION.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='octohatrack',
-    version='1.0.0-alpha',
-    description='Show _all_ the contributors to a GitHub repository',
+    name="octohatrack",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Show _all_ the contributors to a GitHub repository",
     long_description=long_description,
-    url='https://github.com/labhr/octohatrack',
-    author='Katie McLaughlin',
-    author_email='katie@glasnt.com',
-    license='MIT',
+    url="https://github.com/labhr/octohatrack",
+    author="Katie McLaughlin",
+    author_email="katie@glasnt.com",
+    license="MIT",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
     ],
-    keywords='octohatrack github contributions',
-    install_requires=[
-        'requests',
-        'gitpython',
-        'requests-cache',
-    ],
-    entry_points={
-        'console_scripts': [
-            "octohatrack = octohatrack.__main__:main"
-        ]
-    },
-    packages=find_packages()
+    keywords="octohatrack github contributions",
+    install_requires=["requests", "gitpython", "requests-cache",],
+    entry_points={"console_scripts": ["octohatrack = octohatrack.__main__:main"]},
+    packages=find_packages(),
 )
